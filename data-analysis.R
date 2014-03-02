f2628775d98edb30fbfecdd75fda2d33c8306860
Pedro <zepedro.correia@gmail.com>
2014-03-02 19:21:45 +0100

    Rearranged flow of processing results.

--- data-analysis.R
+++ data-analysis.R
@@ -1,138 +1,69 @@
 source('data-analysis-functions.R')
 library(apcluster)
 
-library(memoise)
-if (!exists('match.all.languages.m')) {
-    match.all.languages.m <- memoise(match.all.languages)
-}
+agreement.level <- 0.5
 
-if (!file.exists("wcs-modes.csv")) {
+cat("Reading data from WCS files...\n")
+wcs.chip <- read.table("WCS-Data-20110316/chip.txt", stringsAsFactors=FALSE, sep="\t", na.strings="",
+                col.names=c("wcs.index","wcs.value","wcs.hue","wcs.valuehue"))
+                    
+wcs.lang <- read.table("WCS-Data-20110316/lang.txt", stringsAsFactors=FALSE, sep="\t", na.strings="")
+colnames(wcs.lang)[1:4] <- c("wcs.index","name","location","field.worker")
 
-    cat("Reading data from WCS files...\n")
-    wcs.chip <- read.table("WCS-Data-20110316/chip.txt", stringsAsFactors=FALSE, sep="\t", na.strings="",
-                    col.names=c("wcs.index","wcs.value","wcs.hue","wcs.valuehue"))
-                        
-    wcs.lang <- read.table("WCS-Data-20110316/lang.txt", stringsAsFactors=FALSE, sep="\t", na.strings="")
-    colnames(wcs.lang)[1:4] <- c("wcs.index","name","location","field.worker")
-    
-    wcs <- read.table("WCS-Data-20110316/term.txt", stringsAsFactors=FALSE, sep="\t", na.strings="",
-               col.names=c("wcs.lang.index","speaker.index", "wcs.chip.index","term"))
-               
-    cat("Read", nrow(wcs), "datapoints.\n")
-               
-    cat("Marking dubious data points (\"?\" and \"*\") as <NA>...\n")
-    wcs$term[wcs$term == "?"] <- NA
-    wcs$term[wcs$term == "*"] <- NA
+wcs <- read.table("WCS-Data-20110316/term.txt", stringsAsFactors=FALSE, sep="\t", na.strings="",
+           col.names=c("wcs.lang.index","speaker.index", "wcs.chip.index","term"))
+           
+cat("Read", nrow(wcs), "datapoints.\n")
+           
+cat("Marking dubious data points (\"?\" and \"*\") as <NA>...\n")
+wcs$term[wcs$term == "?"] <- NA
+wcs$term[wcs$term == "*"] <- NA
 
-    cat("Calculating mode map for each language (may take a while)...\n")
-    wcs.modes <- aggregate(wcs$term, by=list(wcs$wcs.chip.index, wcs$wcs.lang.index), FUN=mode.statistic)
-    colnames(wcs.modes) <- c("wcs.chip.index", "wcs.lang.index", "term")
+cat("Calculating mode map for each language (may take a while)...\n")
+wcs.modes <- aggregate(wcs$term, by=list(wcs$wcs.chip.index, wcs$wcs.lang.index),
+                       FUN=function(x) { agreement(x, min.level=agreement.level) }) 
+colnames(wcs.modes) <- c("wcs.chip.index", "wcs.lang.index", "term")
 
-    wcs.modes$wcs.hue <- wcs.chip$wcs.hue[wcs.modes$wcs.chip.index]
-    wcs.modes$wcs.value <- wcs.chip$wcs.value[wcs.modes$wcs.chip.index]
-    wcs.modes$lang.name <- wcs.lang$name[wcs.modes$wcs.lang.index]
-    
-    wcs.modes$wcs.hue <- factor(as.numeric(wcs.modes$wcs.hue))
-    wcs.modes$wcs.value <- factor(wcs.modes$wcs.value, levels=c("J","I","H","G","F","E","D","C","B","A"))
-    
-    cat("Saving mode map to file \"wcs-modes.csv\"...\n")
-    write.csv(wcs.modes, file="wcs-modes.csv", row.names=FALSE, na="<NA>")
-    
-} else {
+wcs.modes$wcs.hue <- wcs.chip$wcs.hue[wcs.modes$wcs.chip.index]
+wcs.modes$wcs.value <- wcs.chip$wcs.value[wcs.modes$wcs.chip.index]
+wcs.modes$lang.name <- wcs.lang$name[wcs.modes$wcs.lang.index]
 
-    cat("Loading data from file \"wcs-modes.csv\"...\n")
-    wcs.modes <- read.csv("wcs-modes.csv", stringsAsFactors=FALSE, na.strings="<NA>")
-    
-    wcs.modes$wcs.hue <- factor(as.numeric(wcs.modes$wcs.hue))
-    wcs.modes$wcs.value <- factor(wcs.modes$wcs.value, levels=c("J","I","H","G","F","E","D","C","B","A"))
-    
-}
+wcs.modes$wcs.hue <- factor(as.numeric(wcs.modes$wcs.hue))
+wcs.modes$wcs.value <- factor(wcs.modes$wcs.value, levels=c("J","I","H","G","F","E","D","C","B","A"))
 
-if (!file.exists("wcs-wcs-match.Rdata")) {
-    cat("Matching WCS languages with WCS languages (may take a while)...\n")
-    wcs.wcs.match <- match.all.languages.m(wcs.modes, wcs.modes)
-    save(wcs.wcs.match, file="wcs-wcs-match.Rdata")
-} else {
-    cat("Loading match for WCS languages with WCS languages from file \"wcs-wcs-match.Rdata\"...\n")
-    load("wcs-wcs-match.Rdata")
-}
+cat("Saving mode map to file \"wcs-modes.csv\"...\n")
+write.csv(wcs.modes, file="wcs-modes.csv", row.names=FALSE, na="<NA>")
+
+cat("Matching WCS languages with WCS languages (may take a while)...\n")
+wcs.wcs.match <- match.all.languages.m(wcs.modes, wcs.modes)
+save(wcs.wcs.match, file="wcs-wcs-match.Rdata")
 
 cat("Reading all simulation results...\n")
-reg3.modes <- read.simulation("results/Regier3.csv")
-reg4.modes <- read.simulation("results/Regier4.csv")
-reg5.modes <- read.simulation("results/Regier5.csv")
-reg6.modes <- read.simulation("results/Regier6.csv")
+sim.modes <- data.frame()
+sim.modes <- rbind(sim.modes, read.simulation("results/Regier3.csv", names.prefix='RKK3.'))
+sim.modes <- rbind(sim.modes, read.simulation("results/Regier4.csv", names.prefix='RKK4.'))
+sim.modes <- rbind(sim.modes, read.simulation("results/Regier5.csv", names.prefix='RKK5.'))
+sim.modes <- rbind(sim.modes, read.simulation("results/Regier6.csv", names.prefix='RKK6.'))
 
-sim3.modes <- read.simulation("results/simulations-3.csv")
-sim4.modes <- read.simulation("results/simulations-4.csv")
-sim5.modes <- read.simulation("results/simulations-5.csv")
-sim6.modes <- read.simulation("results/simulations-6.csv")
-sim7.modes <- read.simulation("results/simulations-7.csv")
-sim8.modes <- read.simulation("results/simulations-8.csv")
+sim.modes <- rbind(sim.modes, read.simulation("results/simulations-3.csv", names.prefix='CO3.'))
+sim.modes <- rbind(sim.modes, read.simulation("results/simulations-4.csv", names.prefix='CO4.'))
+sim.modes <- rbind(sim.modes, read.simulation("results/simulations-5.csv", names.prefix='CO5.'))
+sim.modes <- rbind(sim.modes, read.simulation("results/simulations-6.csv", names.prefix='CO6.'))
+sim.modes <- rbind(sim.modes, read.simulation("results/simulations-7.csv", names.prefix='CO7.'))
+sim.modes <- rbind(sim.modes, read.simulation("results/simulations-8.csv", names.prefix='CO8.'))
 
-sph3.modes <- read.simulation("results/sphere-3.csv")
-sph4.modes <- read.simulation("results/sphere-4.csv")
-sph5.modes <- read.simulation("results/sphere-5.csv")
-sph6.modes <- read.simulation("results/sphere-6.csv")
-sph7.modes <- read.simulation("results/sphere-7.csv")
-sph8.modes <- read.simulation("results/sphere-8.csv")
-
-rand3.modes <- read.simulation("results/random-3.csv")
-rand4.modes <- read.simulation("results/random-4.csv")
-rand5.modes <- read.simulation("results/random-5.csv")
-rand6.modes <- read.simulation("results/random-6.csv")
-rand7.modes <- read.simulation("results/random-7.csv")
-rand8.modes <- read.simulation("results/random-8.csv")
-
-cat("Matching Regier et al results with WCS languages...\n")
-reg3.wcs.match <- match.all.languages.m(reg3.modes, wcs.modes)
-reg4.wcs.match <- match.all.languages.m(reg4.modes, wcs.modes)
-reg5.wcs.match <- match.all.languages.m(reg5.modes, wcs.modes)
-reg6.wcs.match <- match.all.languages.m(reg6.modes, wcs.modes)
-
-cat("Matching simulation results with WCS languages...\n")
-sim3.wcs.match <- match.all.languages.m(sim3.modes, wcs.modes)
-sim4.wcs.match <- match.all.languages.m(sim4.modes, wcs.modes)
-sim5.wcs.match <- match.all.languages.m(sim5.modes, wcs.modes)
-sim6.wcs.match <- match.all.languages.m(sim6.modes, wcs.modes)
-sim7.wcs.match <- match.all.languages.m(sim7.modes, wcs.modes)
-sim8.wcs.match <- match.all.languages.m(sim8.modes, wcs.modes)
-
-cat("Matching simulation results with perfect sphere with WCS languages...\n")
-sph3.wcs.match <- match.all.languages.m(sph3.modes, wcs.modes)
-sph4.wcs.match <- match.all.languages.m(sph4.modes, wcs.modes)
-sph5.wcs.match <- match.all.languages.m(sph5.modes, wcs.modes)
-sph6.wcs.match <- match.all.languages.m(sph6.modes, wcs.modes)
-sph7.wcs.match <- match.all.languages.m(sph7.modes, wcs.modes)
-sph8.wcs.match <- match.all.languages.m(sph8.modes, wcs.modes)
-
-cat("Matching random results with WCS languages...\n")
-rand3.wcs.match <- match.all.languages.m(rand3.modes, wcs.modes)
-rand4.wcs.match <- match.all.languages.m(rand4.modes, wcs.modes)
-rand5.wcs.match <- match.all.languages.m(rand5.modes, wcs.modes)
-rand6.wcs.match <- match.all.languages.m(rand6.modes, wcs.modes)
-rand7.wcs.match <- match.all.languages.m(rand7.modes, wcs.modes)
-rand8.wcs.match <- match.all.languages.m(rand8.modes, wcs.modes)
+cat("Matching simulation results with WCS languages (may take a while)...\n")
+sim.wcs.match <- match.all.languages.m(sim.modes, wcs.modes)
 
 cat("Calculating clustering of WCS languages...\n")
 clustering <- apcluster(s=wcs.wcs.match)
 clustering.eval <- evaluate.clustering(clustering=clustering, similarity.matrix=wcs.wcs.match)
+clustering.summary <- summary.similarities(list(clustering.eval))
 
 cat("Evaluating simulations against clustering...\n")
 simulations.eval <- list()
-simulations.eval[['Reg3']] <- evaluate.language(clustering=clustering, similarity.row=reg3.wcs.match)
-simulations.eval[['Reg4']] <- evaluate.language(clustering=clustering, similarity.row=reg4.wcs.match)
-simulations.eval[['Reg5']] <- evaluate.language(clustering=clustering, similarity.row=reg5.wcs.match)
-simulations.eval[['Reg6']] <- evaluate.language(clustering=clustering, similarity.row=reg6.wcs.match)
-for(sim in 1:nrow(sim3.wcs.match)) simulations.eval[[paste(sep='', 'sim3.', sim)]] <- evaluate.language(clustering=clustering, similarity.row=sim3.wcs.match[sim,])
-for(sim in 1:nrow(sim4.wcs.match)) simulations.eval[[paste(sep='', 'sim4.', sim)]] <- evaluate.language(clustering=clustering, similarity.row=sim4.wcs.match[sim,])
-for(sim in 1:nrow(sim5.wcs.match)) simulations.eval[[paste(sep='', 'sim5.', sim)]] <- evaluate.language(clustering=clustering, similarity.row=sim5.wcs.match[sim,])
-for(sim in 1:nrow(sim6.wcs.match)) simulations.eval[[paste(sep='', 'sim6.', sim)]] <- evaluate.language(clustering=clustering, similarity.row=sim6.wcs.match[sim,])
-for(sim in 1:nrow(sim7.wcs.match)) simulations.eval[[paste(sep='', 'sim7.', sim)]] <- evaluate.language(clustering=clustering, similarity.row=sim7.wcs.match[sim,])
-for(sim in 1:nrow(sim8.wcs.match)) simulations.eval[[paste(sep='', 'sim8.', sim)]] <- evaluate.language(clustering=clustering, similarity.row=sim8.wcs.match[sim,])
-for(sim in 1:nrow(sph3.wcs.match)) simulations.eval[[paste(sep='', 'sph3.', sim)]] <- evaluate.language(clustering=clustering, similarity.row=sph3.wcs.match[sim,])
-for(sim in 1:nrow(sph4.wcs.match)) simulations.eval[[paste(sep='', 'sph4.', sim)]] <- evaluate.language(clustering=clustering, similarity.row=sph4.wcs.match[sim,])
-for(sim in 1:nrow(sph5.wcs.match)) simulations.eval[[paste(sep='', 'sph5.', sim)]] <- evaluate.language(clustering=clustering, similarity.row=sph5.wcs.match[sim,])
-for(sim in 1:nrow(sph6.wcs.match)) simulations.eval[[paste(sep='', 'sph6.', sim)]] <- evaluate.language(clustering=clustering, similarity.row=sph6.wcs.match[sim,])
-for(sim in 1:nrow(sph7.wcs.match)) simulations.eval[[paste(sep='', 'sph7.', sim)]] <- evaluate.language(clustering=clustering, similarity.row=sph7.wcs.match[sim,])
-for(sim in 1:nrow(sph8.wcs.match)) simulations.eval[[paste(sep='', 'sph8.', sim)]] <- evaluate.language(clustering=clustering, similarity.row=sph8.wcs.match[sim,])
+for(sim in 1:nrow(sim.wcs.match)) {
+  simulations.eval[[row.names(sim.wcs.match)[sim]]] <- evaluate.language(clustering=clustering, similarity.row=sim.wcs.match[sim,])
+}
+
+simulations.summary <- summary.similarities(simulations.eval)
