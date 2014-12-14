1f07c5c264c64c0d42d8df87a98b483ac35a9029
Pedro <zepedro.correia@gmail.com>
2014-12-14 23:12:30 +0000

    Changes for a batch run.

--- run-batch.sh
+++ run-batch.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-for t in 3 4 5 6
+for spectrum in 'spectrum-10-CIELAB-Daylight-6500K' 'spectrum-15-CIELAB-Daylight-6500K'
 do
-	results_file="results/new/spectrum-McGill-smoothing-10-$t.csv"
-	measurements_file="results/new/spectrum-McGill-smoothing-10-$t-measurements.csv"
-	> $results_file
-	> $measurements_file
-    echo $t
-	for n in {1..20}
-	do
-        echo "  $n"
-    	python sim_max_signaling.py 'data/spectrum-6500K-5-20-20.csv' 8 $t 99999999999 $results_file $measurements_file
-	done
+    for t in 3 4 5 6
+    do
+        results_file="results/$spectrum-$t.csv"
+        measurements_file="results/$spectrum-$t-measurements.csv"
+        > $results_file
+        > $measurements_file
+        echo $t
+        for n in {1..20}
+        do
+            echo "  $n"
+            python sim_max_signaling.py "data/$spectrum.csv" "data/$spectrum-priors-10.csv" $t 99999999999 $results_file $measurements_file
+            exit 1
+        done
+    done
 done
